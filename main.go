ea2719e123fc7c48fcc7319f5033d726cd3c9979
raisultan <ki.xbozz@gmail.com>
2026-03-22 12:58:43 +0500

    Stage 1: DefaultRules map + fmt print

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
+import "fmt"
+
+var DefaultRules = map[string]string{
+	".jpg":  "Images",
+	".jpeg": "Images",
+	".png":  "Images",
+	".pdf":  "Documents",
+	".doc":  "Documents",
+	".docx": "Documents",
+	".txt":  "Documents",
+	".mp3":  "Music",
+	".wav":  "Music",
+	".mp4":  "Video",
+	".avi":  "Video",
+	".zip":  "Archives",
+	".rar":  "Archives",
+}
+
 func main() {
+	for ext, folder := range DefaultRules {
+		fmt.Printf("Расширение: %s -> Папка: %s\n", ext, folder)
+	}
 }
